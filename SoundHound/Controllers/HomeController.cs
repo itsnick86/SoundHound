fc763e5f37d2af7b6ab1c45c82a4286572137a62
Nick Edwards <nledwa01@gmail.com>
2017-07-03 22:46:31 -0400

    Added the ability to Add a song to the songRepository and View the list of songs

--- SoundHound/Controllers/HomeController.cs
+++ SoundHound/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SoundHound.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,6 +9,7 @@ namespace SoundHound.Controllers
 {
     public class HomeController : Controller
     {
+        static SongRepository _songRepository = new SongRepository();
         public ActionResult Index()
         {
             return View();
@@ -25,6 +27,33 @@ namespace SoundHound.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+
+        public ActionResult CreateSong()
+        {
+            return View("AddSong");
+        }
+
+        public ActionResult AddSong(Song song)
+        {
+            _songRepository.AddSong(song);
+            return View("SingleSong", song);
+        }
+
+        public ActionResult ViewSongs()
+        {
+            return View("SongList", _songRepository._songs);
+        }
+
+        //Class to help store Song list
+        public class SongRepository
+        {
+            public List<Song> _songs = new List<Song>();
+
+            public void AddSong(Song song)
+            {
+                _songs.Add(song);
+            }
         }
     }
 }
---
+++ SoundHound/Models/Song.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+//Model for the AddSong view. It represents the song data that is to be collecected
+
+namespace SoundHound.Models
+{
+    public class Song
+    {
+        public string SongTitle { get; set; }
+
+        public string Artist { get; set; }
+
+        public string Key { get; set; }
+
+        public int BPM { get; set; }
+    }
+}
