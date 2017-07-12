85ecd4065d2102d42df68be8c21ff0cfd1072519
Nick Edwards <nledwa01@gmail.com>
2017-07-11 21:50:53 -0400

    Updated project to use Entity Framework and added a Delete button

--- SoundHound/Controllers/HomeController.cs
+++ SoundHound/Controllers/HomeController.cs
@@ -7,12 +7,12 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Dapper;
+using SoundHound.Repositories;
 
 namespace SoundHound.Controllers
 {
     public class HomeController : Controller
     {
-        static SongRepository _songRepository = new SongRepository();
         public ActionResult Index()
         {
             return View();
@@ -39,70 +39,44 @@ namespace SoundHound.Controllers
 
         public ActionResult AddSong(Song song)
         {
-            _songRepository.AddSong(song);
-            return View("SingleSong", song);
+            using (var repo = new SongRepository())
+            {
+                repo.AddSong(song);
+                return View("SingleSong", song);
+            }
         }
 
         public ActionResult YourSongs()
         {
-            return View("SongList", _songRepository.Songs);
+            using (var repo = new SongRepository())
+            {
+                return View("SongList", repo.Songs.ToList());
+            }
         }
 
         public ActionResult EditSong(int id)
         {
-            return View("EditSong", _songRepository.GetSong(id));
+            using (var repo = new SongRepository())
+            {
+                return View("EditSong", repo.GetSong(id));
+            }
         }
 
         public ActionResult ChangeSong(Song song)
         {
-            _songRepository.EditSong(song);
-            return View("SongList", _songRepository.Songs);
+            using (var repo = new SongRepository())
+            {
+                repo.EditSong(song);
+                return View("SongList", repo.Songs.ToList());
+            }
         }
 
-        //Class to help store Song list
-        public class SongRepository
+        public ActionResult DeleteSong(Song song)
         {
-
-            public void AddSong(Song song)
+            using (var repo = new SongRepository())
             {
-                using (var connection = CreateConnection())
-                {
-                    connection.Execute("INSERT INTO SONGS (SONGTITLE, ARTIST, SONGKEY, BPM) VALUES (@songtitle, @artist, @songkey, @BPM)", 
-                        new { songtitle = song.SongTitle, artist = song.Artist, songkey = song.SongKey, BPM = song.BPM });
-                }
-            }
-
-            public List<Song> Songs
-            {
-                get
-                {
-                    using (var connection = CreateConnection())
-                    {
-                        return connection.Query<Song>("SELECT * FROM SONGS").ToList();
-                    }
-                }
-            }
-
-            public Song GetSong(int id)
-            {
-                using (var connection = CreateConnection())
-                {
-                    return connection.QuerySingle<Song>("SELECT * FROM SONGS WHERE ID = @songid", new { songid = id });
-                }
-            }
-
-            public void EditSong(Song song)
-            {
-                using (var connection = CreateConnection())
-                {
-                    connection.Execute("UPDATE SONGS SET SONGTITLE = @songtitle, ARTIST = @artist, SONGKEY = @songkey, BPM = @BPM where ID = @id", 
-                        new { songtitle = song.SongTitle, artist = song.Artist, songkey = song.SongKey, BPM = song.BPM, id = song.ID });
-                }
-            }
-
-            private IDbConnection CreateConnection()
-            {
-                return new SqlConnection("Server=(localdb)\\mssqllocaldb;Database=SoundHound;Trusted_Connection=True");
+                repo.DeleteSong(song);
+                return View("SongList", repo.Songs.ToList());
             }
         }
     }
---
+++ SoundHound/Repositories/SongRepository.cs
@@ -0,0 +1,47 @@
+﻿using SoundHound.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Diagnostics;
+using System.Linq;
+using System.Web;
+
+namespace SoundHound.Repositories
+{
+    //Class to help store Song list
+    public class SongRepository : DbContext
+    {
+
+        public DbSet<Song> Songs { get; set; }
+
+        public SongRepository() : base("SongRepository")
+        {
+            Debug.Write(Database.Connection.ConnectionString);
+        }
+
+        public void AddSong(Song song)
+        {
+            Songs.Add(song);
+            this.SaveChanges();
+        }
+
+        public Song GetSong(int id)
+        {
+            return (from Song in Songs
+                    where Song.ID == id
+                    select Song).First();
+        }
+
+        public void EditSong(Song song)
+        {
+            this.Entry(song).State = EntityState.Modified;
+            this.SaveChanges();
+        }
+
+        public void DeleteSong(Song song)
+        {
+            this.Entry(song).State = EntityState.Deleted;
+            this.SaveChanges();
+        }
+    }
+}
